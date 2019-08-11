27bd4f7a7fa56e3db849325da4df9869d1b316fb
Ali Rashidi <alirashidi127@gmail.com>
2019-08-11 19:21:12 +0430

    Finished CMake, starting writing library code

---
+++ include/tarnabot/objects/telegramobject.hpp
@@ -0,0 +1,16 @@
+#ifndef TELEGRAMOBJECT_HPP
+#define TELEGRAMOBJECT_HPP
+namespace Tarna
+{
+/*!
+ * \brief The TelegramObject class is the core class to
+ * store an object data.
+ */
+class TelegramObject
+{
+public:
+
+private:
+};
+}
+#endif
