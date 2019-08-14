49542d20b503f5043f9d8e512ffa266fa66ed204
Ali Rashidi <alirashidi127@gmail.com>
2019-08-14 15:19:36 +0430

    Replaced rapidjson with jsoncpp

--- include/tarnabot/objects/telegramobject.hpp
+++ include/tarnabot/objects/telegramobject.hpp
@@ -1,16 +1,38 @@
 #ifndef TELEGRAMOBJECT_HPP
 #define TELEGRAMOBJECT_HPP
+
+#include <json-c/json_object.h>
+
+using rapidjson::Document;
+
 namespace Tarna
 {
 /*!
- * \brief The TelegramObject class is the core class to
- * store an object data.
+ * \brief The TelegramObject class provides an interface for creating
+ * bot objects
  */
 class TelegramObject
 {
 public:
+    /*!
+     * \brief TelegramObject constructor
+     * Constructs an empty telegram object
+     */
+    explicit TelegramObject();
+    /*!
+     * \brief TelegramObject constructor
+     * Constructs a telegram object using the provided JSON document
+     * \param jsonDocument The JSON document to build the object with
+     */
+    explicit TelegramObject(int i);
 
-private:
+    /*!
+     * \brief toJsonDocument returns the JSON document held by this object
+     * Used for sending requests
+     * \return the JSON document
+     */
+
+protected:
 };
 }
 #endif
---
+++ src/telegramobject.cpp
@@ -0,0 +1,12 @@
+#include "../include/tarnabot/objects/telegramobject.hpp"
+using namespace Tarna;
+
+TelegramObject::TelegramObject() :
+    m_document()
+{}
+
+TelegramObject::TelegramObject(int i) :
+    TelegramObject()
+{
+
+}
